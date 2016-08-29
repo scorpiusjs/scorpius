866620a3cefa5451a9c9cd4266e3124de5912041
rwatts <ryandwatts@gmail.com>
2016-08-29 07:02:19 +0100

    Publish version v0.2.0

--- packages/dashboards/package.js
+++ packages/dashboards/package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	name: 'scorpiusjs:dashboards',
-	version: '0.1.0',
+	version: "0.2.0",
 	// Brief, one-line summary of the package.
 	summary: 'Adds a Dashboard and Widgets to scorpius.',
 	// URL to the Git repository containing the source code for this package.
