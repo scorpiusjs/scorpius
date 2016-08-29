4a899e0d29150e4925c7b90c54e3fea9ceca7655
rwatts <ryandwatts@gmail.com>
2016-08-29 07:02:14 +0100

    Publish version v0.2.0

--- packages/collections/package.js
+++ packages/collections/package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'scorpiusjs:collections',
   summary: 'Meteor collection with some magic',
-  version: '0.1.0',
+  version: "0.2.0",
   git: 'https://github.com/scorpiusjs/scorpius'
 });
 
