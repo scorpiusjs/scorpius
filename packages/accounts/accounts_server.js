9c03fdec1bdf61a33bae547d55d1cad1960b49f3
Ryan Watts <ryandwatts@gmail.com>
2016-10-14 21:18:58 -0700

    Resolved issue with accounts where a match error is thrown upon creating a new user and attaching the admin role. #21

--- packages/accounts/accounts_server.js
+++ packages/accounts/accounts_server.js
@@ -2,18 +2,22 @@
  * Sets the default permissions to new users
  */
 Meteor.users.after.insert(function (userId, doc) {
-  var curUserId = doc._id;
+  var curUserId = doc._id.ops[0]._id;
 
   if (scorpius.adminExists) {
-    // if there is a admin created we will set the default roles.
-    var defaultRoles = Options.get('defaultRoles');
-    Roles.addUserToRoles(curUserId, defaultRoles);
-  } else {
-    // If there is no admin, we will add the admin role to this new user.
-    Roles.addUserToRoles(curUserId, 'admin');
-    // Pass to the client if the admin exists
-    scorpius.adminExists = true;
-    Inject.obj('adminExists', { exists: true });
+	
+		// if there is a admin created we will set the default roles.
+		var defaultRoles = Options.get('defaultRoles');
+		Roles.addUserToRoles(curUserId, defaultRoles);
+  
+	} else {
+	
+		// If there is no admin, we will add the admin role to this new user.
+		Roles.addUserToRoles(curUserId, 'admin');
+
+		// Pass to the client if the admin exists
+		scorpius.adminExists = true;
+		Inject.obj('adminExists', { exists: true });
   }
 });
 
@@ -22,11 +26,12 @@ Meteor.users.after.insert(function (userId, doc) {
  * Pass to the client if there is a admin account
  */
 if (Roles._collection) {
-  scorpius.adminExists = Roles._collection.find({ roles: 'admin' }).count() !== 0;
+	scorpius.adminExists = Roles._collection.find({ roles: 'admin' }).count() !== 0;
 } else {
-  scorpius.adminExists = Meteor.users.find({ roles: 'admin' }).count() !== 0;
+	scorpius.adminExists = Meteor.users.find({ roles: 'admin' }).count() !== 0;
 }
+
 Inject.obj('adminExists', { exists: !!scorpius.adminExists });
-AccountsTemplates.configure({
-  forbidClientAccountCreation: !!scorpius.adminExists
+	AccountsTemplates.configure({
+	forbidClientAccountCreation: !!scorpius.adminExists
 });
