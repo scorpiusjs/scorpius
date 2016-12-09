880ae6fbe931b45addb84fb33006ecb61fe447fb
Ryan Watts <ryandwatts@gmail.com>
2016-12-09 11:43:55 -0700

    whitespace

--- packages/base/package.js
+++ packages/base/package.js
@@ -1,62 +1,60 @@
 Package.describe({
-  name: 'scorpiusjs:base',
-  summary: 'Scorpius',
-  version: "0.3.1",
-  git: 'https://github.com/scorpiusjs/scorpius'
+	name: 'scorpiusjs:base',
+	summary: 'Scorpius',
+	version: "0.3.1",
+	git: 'https://github.com/scorpiusjs/scorpius'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.4.2.3');
+	api.versionsFrom('1.4.2.3');
 
-  api.use([
-    'ecmascript',
-    'meteor-base',
-    'mongo',
-    'standard-minifiers',
-    'ecmascript',
-    'underscore',
-    'spacebars',
-    'blaze-html-templates',
-    'check',
-    'tracker',
-    'session',
-    'nicolaslopezj:options@1.0.1',
-    'nicolaslopezj:reactive-templates@1.2.1',
-    'nicolaslopezj:roles@2.6.2',
-    'nicolaslopezj:router-layer@0.0.11',
-    'aldeed:simple-schema@1.5.3',
-    'aldeed:collection2@2.10.0',
-    'scorpiusjs:lang-en@0.3.1'
-  ]);
+	api.use([
+		'ecmascript',
+		'meteor-base',
+		'mongo',
+		'standard-minifiers',
+		'ecmascript',
+		'underscore',
+		'spacebars',
+		'blaze-html-templates',
+		'check',
+		'tracker',
+		'session',
+		'nicolaslopezj:options@1.0.1',
+		'nicolaslopezj:reactive-templates@1.2.1',
+		'nicolaslopezj:roles@2.6.2',
+		'nicolaslopezj:router-layer@0.0.11',
+		'aldeed:simple-schema@1.5.3',
+		'aldeed:collection2@2.10.0'
+	]);
 
-  api.imply([
-    'tracker',
-    'session',
-    'underscore',
-    'check',
-    'nicolaslopezj:router-layer',
-    'nicolaslopezj:options',
-    'nicolaslopezj:reactive-templates',
-    'nicolaslopezj:roles',
-    'scorpiusjs:lang-en'
-  ]);
+	api.imply([
+		'tracker',
+		'session',
+		'underscore',
+		'check',
+		'nicolaslopezj:router-layer',
+		'nicolaslopezj:options',
+		'nicolaslopezj:reactive-templates',
+		'nicolaslopezj:roles'
+	]);
 
-  api.addFiles([
-    'init.js',
-    'helpers.js',
-    'home-route.js',
-    'layouts.js',
-  ]);
+	api.addFiles([
+		'init.js',
+		'helpers.js',
+		'home-route.js',
+		'layouts.js',
+	]);
 
-  api.addFiles([
-    'helpers_client.js',
-    'links.js'
-  ], 'client');
+	api.addFiles([
+		'helpers_client.js',
+		'links.js'
+	], 'client');
 
-  api.export('scorpius');
-});
+	api.export('scorpius');
+	});
 
 Package.onTest(function(api) {
-  api.use('tinytest');
-  // api.use('scorpiusjs:core');
+	api.use('tinytest');
+	// api.use('scorpiusjs:core');
 });
--- packages/core/package.js
+++ packages/core/package.js
@@ -1,39 +1,39 @@
 Package.describe({
-  name: 'scorpiusjs:core',
-  summary: 'A framework that makes complex as well as simple apps possible with minimal effort',
-  version: "0.3.1",
-  git: 'https://github.com/scorpiusjs/scorpius'
+	name: 'scorpiusjs:core',
+	summary: 'A framework that makes complex as well as simple apps possible with minimal effort',
+	version: "0.3.1",
+	git: 'https://github.com/scorpiusjs/scorpius'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.4.2.3');
+	api.versionsFrom('1.4.2.3');
 
-  api.use([
-    'blaze-html-templates',
-    'ecmascript',
-    'scorpiusjs:base@0.3.1',
-    'scorpiusjs:accounts@0.3.1',
-    'scorpiusjs:config@0.3.1',
-    'scorpiusjs:collections@0.3.1',
-    'scorpiusjs:dictionary@0.3.1',
-    'scorpiusjs:attributes@0.3.1',
-    'scorpiusjs:lang-en@0.3.1'
-    ]);
+	api.use([
+		'blaze-html-templates',
+		'ecmascript',
+		'scorpiusjs:base@0.3.1',
+		'scorpiusjs:accounts@0.3.1',
+		'scorpiusjs:config@0.3.1',
+		'scorpiusjs:collections@0.3.1',
+		'scorpiusjs:dictionary@0.3.1',
+		'scorpiusjs:attributes@0.3.1',
+		'scorpiusjs:lang-en@0.3.1'
+	]);
 
-  api.imply([
-    'scorpiusjs:lang-en',
-    'scorpiusjs:base',
-    'scorpiusjs:accounts',
-    'scorpiusjs:config',
-    'scorpiusjs:collections',
-    'scorpiusjs:dictionary',
-    'scorpiusjs:attributes',
-    ]);
+	api.imply([
+		'scorpiusjs:lang-en',
+		'scorpiusjs:base',
+		'scorpiusjs:accounts',
+		'scorpiusjs:config',
+		'scorpiusjs:collections',
+		'scorpiusjs:dictionary',
+		'scorpiusjs:attributes',
+	]);
 
-  api.export('scorpius');
-});
+		api.export('scorpius');
+	});
 
 Package.onTest(function(api) {
-  api.use('tinytest');
-  // api.use('scorpiusjs:core');
+	api.use('tinytest');
+	// api.use('scorpiusjs:core');
 });
