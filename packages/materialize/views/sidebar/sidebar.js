f0e675e2017d8a9aac3c0a206a04957d7079e7bb
Ryan Watts <ryandwatts@gmail.com>
2017-05-04 15:26:33 -0700

    [materialize] - fixed adminLogo

--- packages/materialize/views/sidebar/sidebar.js
+++ packages/materialize/views/sidebar/sidebar.js
@@ -4,3 +4,10 @@ Template.scorpiusMaterializeSidebar.onRendered(function() {
 		let depend = scorpius.links._collection.find().fetch();
 	});
 });
+
+Template.scorpiusMaterializeSidebar.helpers({
+	adminLogo() {
+		let logo = Options.get('adminLogo') !== undefined ? Options.get('adminLogo') : Options.get('siteName', 'Admin');
+		return logo;
+	}
+});
